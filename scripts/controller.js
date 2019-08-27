d7619fbaf3f9be581ab7205678203107102d43c2
PN25-RicaChristine <ricachristine.debuayan@student.passerellesnumeriques.org>
2019-08-27 17:04:56 +0800

    updating controller.js file

--- scripts/controller.js
+++ scripts/controller.js
@@ -1,5 +1,9 @@
 // Create a client instance
-var client;
+//var client;
+var btnConnect = document.getElementById("btn-connect");
+var btnPublish = document.getElementById("btn-publish");
+//var message = document.getElementById("message").value;
+
 //client = new Paho.Client(location.hostname, Number(location.port), "clientId");
 //client = new Paho.Client("iot.eclipse.org", 443, "clientId");
 client = new Paho.Client("broker.hivemq.com", 8000, "clientId");
@@ -9,17 +13,17 @@ client.onConnectionLost = onConnectionLost;
 client.onMessageArrived = onMessageArrived;
 
 // connect the client
-client.connect({onSuccess:onConnect});
+// client.connect({onSuccess:onConnect});
 
 
 // called when the client connects
 function onConnect() {
   // Once a connection has been made, make a subscription and send a message.
-  console.log("onConnect");
+  console.log("Connected successfully...");
   client.subscribe("World");
-  message = new Paho.Message("Hello PN!");
-  message.destinationName = "World";//topic to subscribe
-  client.send(message);
+//    message = new Paho.Message("Hello Rica Christine!");
+//    message.destinationName = "World";//topic to subscribe
+//    client.send(message);
 }
 
 // called when the client loses its connection
@@ -33,3 +37,18 @@ function onConnectionLost(responseObject) {
 function onMessageArrived(message) {
   console.log("onMessageArrived:"+message.payloadString);
 }
+
+btnConnect.addEventListener('click',function(e){
+    e.preventDefault();
+    console.log("Connect button..");
+    // connect the client
+    client.connect({onSuccess:onConnect});
+});
+
+btnPublish.addEventListener('click',function(e){
+    e.preventDefault();
+    console.log("Publish button");
+    message = new Paho.Message("Hello");
+    message.destinationName = "World";//topic to subscribe
+    client.send(message);
+});
